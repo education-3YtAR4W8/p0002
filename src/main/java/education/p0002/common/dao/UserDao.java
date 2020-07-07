e63f02debb30ac3aaa2722071ca6c38ba99bf4a9
temp <temp>
2020-07-08 05:32:44 +0900

    temp

--- src/main/java/education/p0002/common/dao/UserDao.java
+++ src/main/java/education/p0002/common/dao/UserDao.java
@@ -1,9 +1,18 @@
 package education.p0002.common.dao;
 
+import education.p0002.common.entity.User;
 import org.seasar.doma.Dao;
+import org.seasar.doma.Select;
 import org.seasar.doma.boot.ConfigAutowireable;
+
+import java.util.List;
 
 @Dao
 @ConfigAutowireable
 public interface UserDao {
+    @Select
+    List<User> selectAll();
+
+    @Select
+    List<String> selectName(String id);
 }
--- src/main/java/education/p0002/group/GroupController.java
+++ src/main/java/education/p0002/group/GroupController.java
@@ -1,25 +1,69 @@
 package education.p0002.group;
 
+import education.p0002.common.dao.GroupDao;
+import education.p0002.common.dao.GroupUserDao;
+import education.p0002.common.dao.UserDao;
+import education.p0002.common.entity.Group;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class GroupController {
+    @Autowired
+    GroupDao groupDao;
+    @Autowired
+    GroupUserDao groupUserDao;
+    @Autowired
+    UserDao userDao;
+
     @GetMapping(path = "group")
     String group(Model model) {
         Page page = new Page();
 
-
+        page.items = getSearchedItems();
 
         model.addAttribute("page", page);
-        return "search";
+        return "group";
+    }
+
+    List<SearchedItem> getSearchedItems() {
+        List<Group> groupList = groupDao.selectAll();
+        List<SearchedItem> resultList = new ArrayList<>();
+
+        List<String> userNameList = null;
+        for(Group group : groupList){
+            userNameList = userDao.selectName(group.getId());
+
+
+            resultList.add(
+                    new SearchedItem(
+                            group.getName(),
+                            userNameList
+                    )
+            );
+
+        }
+
+        return resultList;
     }
 
     @Getter
     @Setter
     static public class Page {
+        private List<SearchedItem> items = new ArrayList<>();
+    }
+    @AllArgsConstructor
+    @Getter
+    static public class SearchedItem {
+        private String groupName;
+        private List<String> userNames;
     }
 }
